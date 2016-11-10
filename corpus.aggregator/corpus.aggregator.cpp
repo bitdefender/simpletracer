881274f4e10ad42281904f1f61b62ae5fbd4cc0c
Teodor STOENESCU <tstoenescu@bitdefender.com>
2016-11-10 14:34:27 +0200

    Added a corpus aggregator

---
+++ corpus.aggregator/corpus.aggregator.cpp
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <string.h>
+#include "windirent.h"
+
+struct CorpusItemHeader {
+	char fName[60];
+	unsigned int size;
+};
+
+class CorpusWriter {
+private :
+	FILE *fCorpus;
+
+public :
+	CorpusWriter(const char *cName) {
+		fCorpus = fopen(cName, "wt");
+	}
+
+	~CorpusWriter() {
+		fclose(fCorpus);
+	}
+
+	bool AddItem(const char *fName, unsigned char *data, unsigned int size) {
+		CorpusItemHeader header;
+		memset(&header, 0, sizeof(header));
+		strncpy(header.fName, fName, sizeof(header.fName) - 1);
+		header.size = size;
+
+		fwrite(&header, sizeof(header), 1, fCorpus);
+		fwrite(data, 1, size, fCorpus);
+		fflush(fCorpus);
+
+		return true;
+	}
+};
+
+bool BuildCorpus(const char *dir) {
+	DIR *dpdf;
+	dirent *epdf;
+
+	dpdf = opendir(dir);
+	if (dpdf != NULL) {
+		CorpusWriter wr("corpus");
+		
+		while (epdf = readdir(dpdf)) {
+			if (DT_REG == epdf->d_type) {
+				printf("Adding: %s\n", epdf->d_name);
+			
+				FILE *f = fopen(epdf->d_name, "rt");
+				fseek(f, 0L, SEEK_END);
+				long sz = ftell(f);
+				unsigned char *buff = new unsigned char[sz];
+
+				fseek(f, 0, SEEK_SET);
+				fread(buff, 1, sz, f);
+
+				wr.AddItem(epdf->d_name, buff, sz);
+
+				delete buff;
+				fclose(f);
+			}
+			// std::cout << epdf->d_name << std::endl;
+		}
+	}
+
+	return true;
+}
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		printf("Usage %s fileMask\n\n", argv[0]);
+		return 0;
+	}
+
+	BuildCorpus(argv[1]);
+	return 0;
+}
