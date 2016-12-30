c26ec1b84d799f0385605b107a764492d0186c44
marius92mc <marius_92m@yahoo.com>
2016-12-30 22:54:27 +0200

    Added stub script for environment variables
    Considered this improvement
    @teodor-stoenescu wrote
    "Wouldn't it be better if we made this script able
    to run any binary on any corpus? I'm thinking of
    placing all references to directories inside
    environment variables and making some stub
    scripts that set them(the environment variables)
    for a particular corpus dir."

--- utils_symexec/run_example.sh
+++ utils_symexec/run_example.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #
-# Run a testcase from corpus-libhttp/CORPUS-01/
+# Run a testcase from $LD_CORPUS_PATH.
+#
+# $LD_CORPUS_PATH is defined in stub_env_variables.sh.
 #
 
 cd ./build/bin
-export LD_LIBRARY_PATH=`pwd`/../lib
 
-files=(../../corpus-libhttp/CORPUS-01/*)
+files=($LD_CORPUS_PATH/*)
 testcase="${files[RANDOM % ${#files[@]}]}"
 
 printf "Using %s testcase.\n" "$testcase"
@@ -18,4 +19,3 @@ traceSimpleOutFileName="trace.simple.out"
 echo ""
 du -h $executionLogFileName $traceSimpleOutFileName
 echo "execution.log and trace.simple.out can be found in build/bin/"
-
