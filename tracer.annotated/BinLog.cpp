63b79b1beda948d144e579f17a64a9d0a7a49daf
Teodor STOENESCU <tstoenescu@bitdefender.com>
2017-03-27 12:18:25 +0300

    Intermediate commit.

--- tracer.annotated/BinLog.cpp
+++ tracer.annotated/BinLog.cpp
@@ -1,10 +1,24 @@
 #include "BinLog.h"
 
 #include <string.h>
+#include <cstdlib>
 
-BinLogWriter::BinLogWriter(FILE *log) {
+BinLogWriter::BinLogWriter(FILE *log, const bool shouldBufferEntries) {
 	fLog = log;
 	lastModule[0] = '\0';
+
+	bufferingEntries 	= shouldBufferEntries;
+	bufferEntries 		= shouldBufferEntries ? new char[MAX_ENTRIES_BUFFER_SIZE] : nullptr;
+	bufferHeaderPos 	= 0;
+}
+
+BinLogWriter::~BinLogWriter()
+{
+	if (bufferEntries)
+	{
+		delete [] bufferEntries;
+		bufferEntries = nullptr;
+	}
 }
 
 bool BinLogWriter::WriteEntry(const char *module, unsigned int offset, unsigned int cost) {
@@ -19,13 +33,55 @@ bool BinLogWriter::WriteEntry(const char *module, unsigned int offset, unsigned 
 	ble.bbOffset = offset;
 	ble.bbCost = cost;
 
-	fwrite(&ble, sizeof(ble), 1, fLog);
-	if (ble.modNameLength) {
-		fwrite(module, 1, ble.modNameLength, fLog);
+	if (!bufferingEntries)
+	{						
+		fwrite(&ble, sizeof(ble), 1, fLog);
+		if (ble.modNameLength) {
+			fwrite(module, 1, ble.modNameLength, fLog);
+		}
 	}
+	else
+	{
+		if (bufferHeaderPos + sizeof(ble) + ble.modNameLength >= MAX_ENTRIES_BUFFER_SIZE)
+		{
+			fprintf(stderr, "Already reached the end of the buffer :( exiting\n");
+			exit(1);
+		}
 
-	fflush(fLog);
+		memcpy(&bufferEntries[bufferHeaderPos], &ble, sizeof(ble));
+		bufferHeaderPos += sizeof(ble);
+
+		if (ble.modNameLength) {
+			memcpy(&bufferEntries[bufferHeaderPos], module, ble.modNameLength*sizeof(char));
+			bufferHeaderPos += ble.modNameLength*sizeof(char);
+		}
+	}
+	
 	return true;
 }
 
+void BinLogWriter::ExecutionEnd()
+{
+	// If using buffer mode, write it to stdout
+	if (!bufferEntries) { 
+		if (fLog) {
+			fflush(fLog);
+		}
+	}
+	else {
+		if (fLog) {	
+			// Write the total number of bytes of the output buffer, then the buffered data
+			const size_t totalSizeToWrite = sizeof(bufferEntries[0]) * bufferHeaderPos;
+			fwrite(&totalSizeToWrite, sizeof(totalSizeToWrite), 1, fLog);
+			fwrite(bufferEntries, sizeof(bufferEntries[0]), bufferHeaderPos, fLog);
 
+			fflush(fLog);
+		}
+	}
+}
+
+void BinLogWriter::ExecutionBegin()
+{
+	bufferHeaderPos = 0;
+	lastModule[0] = '\0';
+}
