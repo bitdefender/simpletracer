62d7a0e1903a93aac1796426ba5a165cb31713de
Alexandra Sandulescu <alecsandra.sandulescu@gmail.com>
2017-11-20 16:02:25 +0200

    z3 feature: cleanup

--- libtracer/annotated.tracer/z3/Z3SymbolicExecutor.cpp
+++ libtracer/annotated.tracer/z3/Z3SymbolicExecutor.cpp
@@ -120,41 +120,6 @@ void *Z3SymbolicExecutor::ConcatBits(void *expr1, void *expr2) {
 	return (void *)ret;
 }
 
-/*bool IsLocked(void *ctx, const Z3_ast *ast) {
-	void *v = Z3_get_user_ptr((Z3_context)ctx, *ast);
-	const TrackedVariableData *tvd = (const TrackedVariableData *)v;
-	const TrackedVariableData *tmp = tvd;
-
-	do {
-		if (tmp->isLocked) {
-			return true;
-		}
-		tmp = tmp->next;
-	} while (tmp != tvd);
-
-	return false;
-}
-
-void Lock(void *ctx, Z3_ast *ast) {
-	void *v = Z3_get_user_ptr((Z3_context)ctx, *ast);
-	TrackedVariableData *tvd = (TrackedVariableData *)v;
-	TrackedVariableData *tmp = tvd;
-	do {
-		tmp->isLocked = true;
-		tmp = tmp->next;
-	} while (tmp != tvd);
-}
-
-void Unlock(void *ctx, Z3_ast *ast) {
-	void *v = Z3_get_user_ptr((Z3_context)ctx, *ast);
-	TrackedVariableData *tvd = (TrackedVariableData *)v;
-	TrackedVariableData *tmp = tvd;
-	do {
-		tmp->isLocked = true;
-		tmp = tmp->next;
-	} while (tmp != tvd);
-}*/
-
 const unsigned int Z3SymbolicExecutor::Z3SymbolicCpuFlag::lazyMarker = 0xDEADBEEF;
 
 /*====================================================================================================*/
@@ -213,36 +178,19 @@ Z3SymbolicExecutor::~Z3SymbolicExecutor() {
 }
 
 void Z3SymbolicExecutor::StepForward() {
-	//variableTracker.Forward();
 	Z3_solver_push(context, solver);
 	for (int i = 0; i < 7; ++i) {
 		lazyFlags[i]->SaveState(*ls);
 	}
-
-	/*for (int i = 0; i < 8; ++i) {
-		subRegisters[i].SaveState(*ls);
-	}*/
-
-	//printf("Solver push\n");
 }
 
 void Z3SymbolicExecutor::StepBackward() {
-	/*for (int i = 7; i >= 0; --i) {
-		subRegisters[i].LoadState(*ls);
-	}*/
-
 	for (int i = 6; i >= 0; --i) {
 		lazyFlags[i]->LoadState(*ls);
 	}
 
 	Z3_solver_pop(context, solver, 1);
-	//variableTracker.Backward();
-	//printf("Solver pop\n");
 }
-
-//void Z3SymbolicExecutor::Lock(Z3_ast t) {
-	//variableTracker.Lock(&t);
-//}
 
 void Z3SymbolicExecutor::SymbolicExecuteUnk(RiverInstruction *instruction, SymbolicOperands *ops) {
 	DEBUG_BREAK;
@@ -264,7 +212,6 @@ Z3_ast Z3SymbolicExecutor::ExecuteAdd(Z3_ast o1, Z3_ast o2) {
 	Z3_ast r = Z3_mk_bvadd(context, o1, o2);
 	env->SetOperand(0, r);
 
-	//printf("<sym> add %p <= %p, %p\n", r, o1, o2);
 	printf("<sym> add op1 %s",
 		Z3_ast_to_string(context, o1)
 	);
@@ -283,13 +230,11 @@ Z3_ast Z3SymbolicExecutor::ExecuteOr(Z3_ast o1, Z3_ast o2) {
 }
 
 Z3_ast Z3SymbolicExecutor::ExecuteAdc(Z3_ast o1, Z3_ast o2) {
-	//return Z3_mk_bvadd(context, o1, o2);
 	DEBUG_BREAK;
 	return nullptr;
 }
 
 Z3_ast Z3SymbolicExecutor::ExecuteSbb(Z3_ast o1, Z3_ast o2) {
-	//return Z3_mk_bvor(context, o1, o2);
 	DEBUG_BREAK;
 	return nullptr;
 }
@@ -318,7 +263,6 @@ Z3_ast Z3SymbolicExecutor::ExecuteXor(Z3_ast o1, Z3_ast o2) {
 	Z3_ast r = Z3_mk_bvxor(context, o1, o2);
 	env->SetOperand(0, r);
 
-	//printf("<sym> xor %p <= %p, %p\n", r, o1, o2);
 	printf("<sym> xor op1 %s",
 		Z3_ast_to_string(context, o1)
 	);
@@ -330,7 +274,6 @@ Z3_ast Z3SymbolicExecutor::ExecuteXor(Z3_ast o1, Z3_ast o2) {
 
 Z3_ast Z3SymbolicExecutor::ExecuteCmp(Z3_ast o1, Z3_ast o2) {
 	Z3_ast r = Z3_mk_bvsub(context, o1, o2);
-	//printf("<sym> cmp %p <= %p, %p\n", r, o1, o2);
 	printf("<sym> cmp op1 %s",
 		Z3_ast_to_string(context, o1)
 	);
